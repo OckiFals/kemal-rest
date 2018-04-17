f82b6bd37988d4417d33459483dfdc2fafd8e76f
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2018-04-17 11:41:09 +0700

    Change db schema.

--- kemal-rest.sql
+++ kemal-rest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 16, 2018 at 09:51 AM
+-- Generation Time: Apr 17, 2018 at 04:40 AM
 -- Server version: 5.7.21
 -- PHP Version: 7.1.14
 
@@ -29,9 +29,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `author` (
-  `id` int(11) NOT NULL,
+  `id` bigint(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `nationality` varchar(255) NOT NULL
+  `nationality` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -41,8 +43,8 @@ CREATE TABLE `author` (
 --
 
 CREATE TABLE `book` (
-  `id` int(11) NOT NULL,
-  `author_id` int(11) NOT NULL,
+  `id` bigint(11) NOT NULL,
+  `author_id` bigint(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `year` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -72,13 +74,13 @@ ALTER TABLE `book`
 -- AUTO_INCREMENT for table `author`
 --
 ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -88,7 +90,7 @@ ALTER TABLE `book`
 -- Constraints for table `book`
 --
 ALTER TABLE `book`
-  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
